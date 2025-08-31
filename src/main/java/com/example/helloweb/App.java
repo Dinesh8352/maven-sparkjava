385590cf5f4ba0579c09e7876d65c3f0823902f5
Dinesh8352 <166284817+Dinesh8352@users.noreply.github.com>
2025-08-31 23:26:26 +0530

    Update App.java

--- src/main/java/com/example/helloweb/App.java
+++ src/main/java/com/example/helloweb/App.java
@@ -5,6 +5,6 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         port(8080); // Listen on port 8080
-        get("/", (req, res) -> "Hello from EC2 Web App!. Welcome to Docker sessions.");
+        get("/", (req, res) -> "Hello from EC2 Web App!. Welcome to Docker sessions. Dinesh configured the file in gitbash at 11:40PM");
     }
 }
